76f2ed26ccdc0c1500184d1a9161bdccf6c52883
xa-xa-xa <28684394+xa-xa-xa@users.noreply.github.com>
2018-08-21 16:53:02 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {
+  Component
+} from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return ( <
+      div className = "App"       >
+      <h1 > The app Component!!! < /h1 > <
+      /div >
+      <label
+
+    );
+  }
+}
+
+
+
+export default App;
