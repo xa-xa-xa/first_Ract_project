95519cbf8132f90c6d8182516d0721f025109cf0
xa-xa-xa <28684394+xa-xa-xa@users.noreply.github.com>
2018-08-27 09:03:14 -0700

    Add files via upload

---
+++ ContactManager/App.js
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return ( <div className = "App">
+            <h1> The app Component!!! </h1 >
+       </div>
+    );
+  }
+}
+
+
+
+export default App;
